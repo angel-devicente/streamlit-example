41a205ee19ef767761a4341cd8fcaee969296801
Angel de Vicente <angel.de.vicente@iac.es>
2022-12-19 22:52:56 +0000

    tes

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ idx = st.slider("Mammography image number", 1, 200, 100)
 url = 'https://ad1p7rqhyj.execute-api.eu-west-2.amazonaws.com/test/predict'  
 data = {'url': 'https://i.ibb.co/ZXs9SJN/mdb182.jpg'}                      
 
-result = requests.post(url, json=data).json() 
-
-result
+if st.button('Predict'):
+    result = requests.post(url, json=data).json() 
+    result
 
