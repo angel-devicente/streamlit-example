9f82e392d8c95098ce7ab25f3c372dba7d8fb2d8
Angel de Vicente <angel.de.vicente@iac.es>
2022-12-19 22:50:17 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+#from collections import namedtuple
+#import altair as alt
+#import math
+#import pandas as pd
 import streamlit as st
 import requests
 
@@ -9,16 +9,13 @@ import requests
 # ML-Zoomcamp Capstone Project
 
 """
-                                                                                                        
 
+idx = st.slider("Mammography image number", 1, 200, 100)
 
-with st.echo(code_location='below'):
-    idx = st.slider("Mammography image number", 1, 200, 100)
+url = 'https://ad1p7rqhyj.execute-api.eu-west-2.amazonaws.com/test/predict'  
+data = {'url': 'https://i.ibb.co/ZXs9SJN/mdb182.jpg'}                      
 
-    url = 'https://ad1p7rqhyj.execute-api.eu-west-2.amazonaws.com/test/predict'  
-    data = {'url': 'https://i.ibb.co/ZXs9SJN/mdb182.jpg'}                      
-                                                                           
-    result = requests.post(url, json=data).json() 
+result = requests.post(url, json=data).json() 
 
-    result
+result
 
