e4415cbdb7f3f403ec46189026c1480816f296ea
Angel de Vicente <angel.de.vicente@iac.es>
2022-12-20 10:26:36 +0000

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,10 @@ idx = st.slider("Mammography image number", 1, 2, 1)
 url = 'https://ad1p7rqhyj.execute-api.eu-west-2.amazonaws.com/test/predict'  
 data = {'url': 'https://mammography-test.s3.eu-west-2.amazonaws.com/mdb002.jpg'}                      
 
+st.image(
+    "https://mammography-test.s3.eu-west-2.amazonaws.com/mdb002.jpg",
+    width=400, # Manually Adjust the width of the image as per requirement
+)
 
 if st.button('Predict'):
     result = requests.post(url, json=data).json() 
